eb465db2d6f4f8add26efd2de7eed044e86c88e2
roland-5 <44477799+roland-5@users.noreply.github.com>
2026-01-20 08:54:52 +0100

    aktualizacja do nowej składni

--- rules.mk
+++ rules.mk
@@ -1,5 +1,27 @@
+# MCU
+MCU = cortex-m0plus
+ARMV = 6
+USE_FPU = no
+MCU_FAMILY = HT32
+MCU_SERIES = HT32F523xx
+MCU_LDSCRIPT = HT32F52352_ANNEPRO2_C18
+MCU_STARTUP = ht32f523xx
+
+BOARD = ANNEPRO2_C18
+
+# Bootloader selection
+BOOTLOADER = custom
+PROGRAM_CMD = annepro2_tools --boot $(BUILD_DIR)/$(TARGET).bin
+
 # Custom RGB matrix handling
 RGB_MATRIX_ENABLE = no
 MOUSEKEY_ENABLE = yes
 KEY_LOCK_ENABLE = yes
 NKRO_ENABLE = yes
+
+# Anne Pro 2
+SRC = \
+	annepro2_ble.c \
+	ap2_led.c \
+	protocol.c \
+	rgb_driver.c \
